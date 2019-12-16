a0fce8c9e72ba2db396bf2ad408b591d371f8e04
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-12-16 13:11:02 -0800

    Create User type

---
+++ src/schema.js
@@ -0,0 +1,39 @@
+const { gql } = require("apollo-server");
+
+module.exports = gql`
+  """
+  Queries
+  """
+  type Query {
+    """
+    Me represents the current user based off of the authorization token sent from headers
+    """
+    me: User!
+  }
+
+  # """
+  # Mutations
+  # """
+  # # type Mutation {
+
+  # # }
+
+  """
+  User Type
+  """
+  type User {
+    email: String!
+    password: String!
+    username: String!
+    firstName: String!
+    lastName: String!
+    state: String!
+    city: String!
+    zip: Int!
+    address: String!
+    address2: String!
+    country: String!
+    phoneNumber: Int!
+    profilePicture: String!
+  }
+`;
