3e5d002f2a81d72a3c051046958162b13a038634
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-12-19 01:55:25 -0800

    Create createUser mutation

--- src/resolvers/mutations/User.js
+++ src/resolvers/mutations/User.js
@@ -1,7 +1,36 @@
+const argon2 = require("argon2");
+
 /*
  * User Mutations
  */
 
 module.exports = {
-  createUser(_, args, { photon }, info) {}
+  async createUser(_, args, { photon }, info) {
+    // Check password length
+    if (args.data.password.length < 8)
+      throw new Error("Password must be 8 characters or longer");
+
+    /*
+     * Hash and Salt password
+     * Argon2 Wiki - https://github.com/ranisalt/node-argon2/wiki/Options
+     */
+    const hashedPassword = await argon2.hash(args.data.password, {
+      type: argon2.argon2id,
+      hashLength: 64
+    });
+
+    // Create user
+    const user = await photon.users.create({
+      data: {
+        ...args.data,
+        password: hashedPassword
+      }
+    });
+
+    // Generate jwt token
+    const token = generateToken(user.id);
+
+    // Return AuthPayload
+    return { token, user };
+  }
 };
