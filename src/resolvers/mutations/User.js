67a07129c1968387ec0679467680af5d26260ae9
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-12-20 13:46:21 -0800

    Create loginUser mutation

--- src/resolvers/mutations/User.js
+++ src/resolvers/mutations/User.js
@@ -5,13 +5,13 @@ const argon2 = require("argon2");
  */
 
 module.exports = {
-  async createUser(_, args, { photon }, info) {
-    // Check password length
+  async createUser(_, args, { photon }) {
+    // check password length
     if (args.data.password.length < 8)
       throw new Error("Password must be 8 characters or longer");
 
     /*
-     * Hash and Salt password
+     * hash and salt password
      * Argon2 Wiki - https://github.com/ranisalt/node-argon2/wiki/Options
      */
     const hashedPassword = await argon2.hash(args.data.password, {
@@ -20,7 +20,7 @@ module.exports = {
       hashLength: 64
     });
 
-    // Create user
+    // create user
     const user = await photon.users.create({
       data: {
         ...args.data,
@@ -28,10 +28,43 @@ module.exports = {
       }
     });
 
-    // Generate jwt token
+    // generate jwt token
     const token = generateToken(user.id);
 
-    // Return AuthPayload
+    // return AuthPayload
+    return { token, user };
+  },
+  async loginUser(_, args, { photon }) {
+    // query for user by email and username
+    const user = await photon.users.findMany({
+      where: {
+        OR: [
+          {
+            email: args.data.emailOrUsername
+          },
+          {
+            username: args.data.emailOrUsername
+          }
+        ]
+      }
+    });
+
+    // check existence of user
+    if (!user.length > 0) throw new Error("Incorrect email and or password");
+
+    // validate password
+    const validPassword = await argon2.verify(
+      user[0].password,
+      args.data.password
+    );
+
+    // check validity of password
+    if (!validPassword) throw new Error("Incorrect email and or password");
+
+    // generate token
+    const token = await generateToken(user[0].id);
+
+    // return auth payload
     return { token, user };
   }
 };
