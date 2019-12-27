529078ae5e7764b6663c8f4f6505e9672c41d29c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-12-27 05:44:36 -0800

    createPost mutation wip

--- src/resolvers/mutations/UserPost.js
+++ src/resolvers/mutations/UserPost.js
@@ -1,7 +1,30 @@
+const getUserId = require("../../utils/getUserId");
 const s3Upload = require("../../utils/s3Upload");
 
 module.exports = {
+  // CREATE POST - WIP
   async createPost(_, args, { photon, request }, info) {
-    await s3Upload(args.data.media[0]);
+    const userId = getUserId(request);
+
+    // upload post media to s3
+    const media = await Promise.all(
+      args.data.media.map(file => s3Upload(file))
+    );
+
+    // create post
+    return photon.posts.create(
+      {
+        data: {
+          author: {
+            connect: {
+              id: userId
+            }
+          },
+          media,
+          description: args.data.description
+        }
+      },
+      info
+    );
   }
 };
