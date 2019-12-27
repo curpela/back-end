40d1a252417c7c13f91f68ed8335ced1bd65929f
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-12-27 05:25:43 -0800

    Finish s3Upload util
    s3Upload can be used to upload and type of file to a s3 bucket. Every uploaded file automatically has a uniquely generated name

--- src/utils/s3Upload.js
+++ src/utils/s3Upload.js
@@ -1,11 +1,11 @@
 const aws = require("aws-sdk");
-const util = require("util");
-const fs = require("fs");
 const uuid = require("uuid/v4");
 
 /*
- * WIP - NOT PROD READY
- *
+ * s3 Upload
+ * @param: File
+ * @description: Streams file to s3 bucket
+ * @limits: None - Able to upload any file type to s3 bucket
  */
 
 const s3Upload = async file => {
@@ -17,37 +17,54 @@ const s3Upload = async file => {
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     });
 
+    // destructure file obj
     let { createReadStream, filename, mimetype, encoding } = await file;
+
+    // generate filename
     filename = _generateFilename(filename, mimetype);
 
-    const result = await new Promise(async (resolve, reject) => {
-      const stream = createReadStream();
+    //create readable stream
+    /*
+     *  Introduction to nodejs streams - https://flaviocopes.com/nodejs-streams/
+     */
+    const stream = await createReadStream();
 
-      const up = await s3
-        .upload({
-          Key: filename,
-          Bucket: process.env.AWS_BUCKET_NAME,
-          Body: stream,
-          ContentType: mimetype
-        })
-        .promise();
+    // upload to s3
+    const upload = await s3
+      .upload({
+        Key: filename,
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Body: stream,
+        ContentType: mimetype
+      })
+      .promise();
 
-      return resolve(`http://d2lvnywid6p0z4.cloudfront.net/${filename}`);
-    });
-
-    return result;
+    // return uploaded file
+    return {
+      url: `${process.env.AWS_CLOUD_FRONT_DOMAIN}/${upload.Key}`,
+      contentType: mimetype
+    };
   } catch (error) {
-    console.log(error);
-    throw new Error("Sorry, but there was an issue uploading that image");
+    throw new Error("Sorry, but there was an issue while uploading to s3");
   }
 };
 
-// Generate unique file
+// cleans and generates unique filename
 function _generateFilename(filename, mimetype) {
-  console.log(filename);
-  const cleanFileName = filename.toLowerCase().replace(/[^a-z0-9]/g, "-");
-  const id = uuid();
-  return `${id}-${cleanFileName}.${mimetype.split("/")[1]}`;
+  // generate id
+  const id = uuid(); //123e4567-e89b-12d3-a456-426655440000
+
+  // clean filename - original "c@t! video.mp4"
+  const cleanFileName = filename
+    .substring(0, filename.length - 4) // remove file extension from filename
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "-"); // cleaned - "cat-video"
+
+  // gets file extension from mimetype - mimeType "video/mp4"
+  const fileExtension = mimetype.split("/")[1]; // fileExtension "mp4"
+
+  // construct filename
+  return `${id}-${cleanFileName}.${fileExtension}`; //123e4567-e89b-12d3-a456-426655440000-cat-video.mp4
 }
 
 module.exports = s3Upload;
