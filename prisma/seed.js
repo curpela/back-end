e792fea3b8034ebc598805804022aaba5038874a
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-14 00:44:10 -0800

    Create new seed data

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,6 +2,46 @@ const { Photon } = require("@prisma/photon");
 const photon = new Photon();
 
 async function main() {
+  const skyelar = await photon.users.create({
+	  data: {
+	firstName: "Skyelar",
+	lastName: "Carroll",
+	username: "skyelar",
+	password: "$argon2id$v=19$m=4096,t=10,p=1$kAh+YlFdxmejQSvdv8hYAQ$T9cWnNkkAhE6T5ZuZfCYvp+eqdamAB/7sbWK2EOSh446PGBgZfN4m7QmUKYiXPipGJAQB7TiEEtSrj1osOHXvA",
+	email: "skyelar@curpela.com",
+	posts: {
+		create: [
+			{
+				description: "Malibu",
+				media: {
+					create: {
+					  url: "https://www.toptenrealestatedeals.com/wp-content/uploads/2019/07/wk_5d268d22549d8.jpg",
+					  contentType: "image/jpeg"
+					}
+				}
+			},
+			{
+			  description: "Austin Texas 😍",
+			  media: {
+				  create: {
+						url: "https://www.kbhome.com/assets/images/Community%20Images/Texas/Austin%20-%20San%20Marcos/Mason%20Hills%20-%20Hallmark%20Collection/Mason%20Hills_3125_Ext_1200.jpg?format=jpg",
+						contentType: "image/jpeg"
+				  }
+			  }
+		  },
+		  {
+			  description: "Mediterranean!",
+			  media: {
+				  create: {
+						url: "https://freshome.com/wp-content/uploads/2018/09/Mediterranean-2.jpg",
+						contentType: "image/jpeg"
+					  }
+			  }
+		  }
+		]
+	}
+  }})
+
   await photon.users.create({
     data: {
       firstName: "Anthony",
@@ -9,6 +49,15 @@ async function main() {
       username: "adventurini",
       password: "$argon2id$v=19$m=4096,t=10,p=1$kAh+YlFdxmejQSvdv8hYAQ$T9cWnNkkAhE6T5ZuZfCYvp+eqdamAB/7sbWK2EOSh446PGBgZfN4m7QmUKYiXPipGJAQB7TiEEtSrj1osOHXvA",
 	  email: "anthony@curpela.com",
+	  following: {
+		create: {
+			following: {
+				connect: {
+					id: skyelar.id
+				}
+			}
+		}
+	  },
 	  posts: {
 		  create: [
 			  {
