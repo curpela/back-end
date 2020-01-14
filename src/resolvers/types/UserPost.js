efe4f20812f317c98d285d92ea2e035a32b8a45e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-14 01:53:31 -0800

    Resolve like/comment count

--- src/resolvers/types/UserPost.js
+++ src/resolvers/types/UserPost.js
@@ -18,6 +18,18 @@ module.exports = {
       last: 5
     });
   },
+  likeCount(parent, _, { photon}) {
+	 // TODO: REPLACE WITH ACTUAL AGGREGATION
+	 const likes = await photon.likes.findMany({
+		where: {
+		  post: {
+			  id: parent.id
+		  }
+		}
+	  });
+
+	  return likes.length;
+  },
   comments(parent, _, { photon }) {
     return photon.postComments.findMany({
       where: {
@@ -27,5 +39,17 @@ module.exports = {
       },
       last: 5
     });
-  }
+  },
+  commentCount(parent, _, { photon}) {
+	 // TODO: REPLACE WITH ACTUAL AGGREGATION
+	 const comments = await photon.comments.findMany({
+		where: {
+		  post: {
+			  id: parent.id
+		  }
+		}
+	  });
+
+	  return comments.length;
+},
 };
